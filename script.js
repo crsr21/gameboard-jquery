ff14fc7515921779e3f93dc49301b02095104c96
Carlos Socorro <carlosrsocorro@gmail.com>
2015-04-30 09:30:22 -0400

    added dropdown menu and active bell

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 var main = function() {
+	$('.more-btn').click(function(){
+		$(this).next().toggle();
+	});
 
+	$('.share').click(function(){
+		$(this).next().toggle();
+	});
+
+	$('.glyphicon-bell').click(function(){
+		$(this).toggleClass('active');
+	});
 };
 
 $(document).ready(main);
